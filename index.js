d13483f76b1d7a077d7284d1fd9635009cecc63e
Emir Yunus K <83879349+LeeMajiors@users.noreply.github.com>
2023-10-16 22:00:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import usersRoutes from './routes/users.js'
+import movieRoutes from './routes/movies_func.js'
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use('/movies', movieRoutes)
 
-app.use('/users', usersRoutes)
-
-app.get('/', (req, res) => res.send('hello from home page.'));
+app.get('/', (req, res) => res.send('hello from the movie home page.'));
 
 app.listen(PORT, ()=> console.log(`Server Running on port: http://localhost:${PORT}`));
+
+
---
+++ routes/movies_func.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+ import movies from '../movies.json'assert { type: 'json' }
+
+
+const router = express.Router();
+
+// let movies = []
+
+
+
+router.get('/', (req, res) => {
+    console.log(movies);
+    res.send(movies);
+});
+
+
+router.post('/',(req,res)=>{
+    const movie = req.body;
+
+    movies.push({ ...movie, id: uuidv4() })
+
+    res.send(`Movie with the name ${movie.Title} added to the database!`)
+
+
+});
+
+router.get('/:id',(req, res) =>{
+    const { id } = req.params;
+
+    const foundMovie = movies.find((movie) => movie.id === id);
+
+    res.send(foundMovie);
+});
+
+router.delete('/:id',(req,res) => {
+    const { id } = req.params;
+
+    movies = movies.filter((movie)=> movie.id != id);
+
+    res.send(`Movie with the id ${id} is now deleted from the data base`)
+});
+
+/* i could only get this far. unfortunately this code adds the updates to the database */
+// router.put('/:id', (req, res) => {
+//     const movie = req.body
+//     const movieId = req.params.id;
+//     movies.push({ ...movie})
+//     res.send(`Update movie with title ${movie.Title} and the ID ${movie.id} `);
+// });
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedMovie = req.body;
+
+    movies = movies.map((movie) => {
+        if (movie.id === id) {
+            return { ...movie, ...updatedMovie };
+        } else {
+            return movie;
+        }
+    });
+
+    res.send(`Updated movie with ID ${id}`);
+});
+
+
+
+
+
+
+export default router;
